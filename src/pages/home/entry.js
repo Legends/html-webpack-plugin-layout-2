0956d5b7e72061c357ddcd559698ada797d0003a
Legends <jovan27@gmx.de>
2020-01-14 14:25:15 +0100

    --

--- src/pages/home/entry.js
+++ src/pages/home/entry.js
@@ -1,6 +1,6 @@
 import "../../index";
-import {isDate} from "lodash-es"
-// import isDate from "lodash-es/isDate"
+// import {isDate} from "lodash-es"
+import isDate from "lodash-es/isDate"
  
 
 console.log("I am index");
